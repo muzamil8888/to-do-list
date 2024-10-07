42619560345b0c0fb034b04030f2b0b9f17ead88
muzamil8888 <mmmuzammilmeo456@gmail.com>
2024-10-07 23:26:30 +0500

    first change

--- array.js
+++
@@ -1,8 +0,0 @@
-export {};
-// let fruit = ['banana','apple','mango'];
-// // fruit.push('grappes') 
-// // this is called push method .
-// // fruit.pop() 
-// // is is called pop method.
-// fruit = fruit.concat("grappes","orange","watermelon");
-// console.log(fruit);
--- array.ts
+++
@@ -1,7 +0,0 @@
-// let fruit = ['banana','apple','mango'];
-// // fruit.push('grappes') 
-// // this is called push method .
-// // fruit.pop() 
-// // is is called pop method.
-// fruit = fruit.concat("grappes","orange","watermelon");
-// console.log(fruit);
